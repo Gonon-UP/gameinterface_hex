80480b30e5655628f35ccaeb20ac1be4a5fda865
Gonon <CX-300s_UP>
2024-02-13 00:53:48 -0800

    Preliminary hexagon grid implementation

--- app/src/main/java/com/example/gameinterface_hex/Hex_SurfaceView.java
+++ app/src/main/java/com/example/gameinterface_hex/Hex_SurfaceView.java
@@ -1,21 +1,28 @@
 package com.example.gameinterface_hex;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
-
+import android.graphics.Color;
+import android.graphics.Path;
+import android.graphics.Region;
 
 
 public class Hex_SurfaceView extends SurfaceView    {
 
-
     private Paint brightPink = new Paint();
     private Paint forestGreen = new Paint();
     private Paint teal = new Paint();
+
+    // Size and shape variables
+    private Path hexagonPath;
+    private Path hexagonBorderPath;
+    private float radius;
+    private float width, height;
+    private int maskColor;
+
 
 
     public Hex_SurfaceView(Context context, AttributeSet attrs) {
@@ -24,36 +31,83 @@ public class Hex_SurfaceView extends SurfaceView    {
         forestGreen.setColor(0xFF228B22);
         forestGreen.setStyle(Paint.Style.STROKE);
         teal.setColor(getResources().getColor(R.color.teal_700, null));
-
+        setUp();
         setWillNotDraw(false);
     }
-    // Drag and drop
-    // Zoom in and out
-    // Change color settings
-    // Wizard
-    // Ads
+
+
+
+    /**
+     External Citation
+     Date: 13 February 2024
+     Problem: Could not draw a hexagon on the surface view
+     Resource:
+     https://stackoverflow.com/questions/30217450/how-to-draw-hexagons-in-android
+     Solution: I used the example code from this post as the basis for the code below
+     Only change is the size and position of the hexagon
+     */
+
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawCircle(200.0f, 300.0f, 50.0f, brightPink);
-        canvas.drawRect(300.0f, 100.0f, 350.0f, 175.0f, forestGreen);
+        super.onDraw(canvas);
+        canvas.drawColor(Color.WHITE);
+        canvas.clipPath(hexagonBorderPath, Region.Op.DIFFERENCE);
+        canvas.drawColor(Color.BLACK);
+        canvas.save();
+        canvas.clipPath(hexagonPath, Region.Op.DIFFERENCE);
+        canvas.drawColor(maskColor);
+        canvas.save();
+    }
 
-        // int[] xPoints = {200, 300, 350, 300, 200, 150};
-        // int[] yPoints = {350, 350, 250, 150, 150, 250};
-        // canvas.drawPoint(xPoints, yPoints, xPoints.length);
-        }
-//        for(int i = 1, i < 6, i++){
-//            canvas.drawPoint();
-//        }
 
-        //Create a bitmap oblect that contains the image
-//        Bitmap smileyBitmap =
-//                BitmapFactory.decodeResource(getResources(),
-//                        R.drawable.smiley_cody);
-//
-//        //Resize to 100x100 dp
-//        smileyBitmap = Bitmap.createScaledBitmap(smileyBitmap, 100, 100, false);
-//
-//        //draw the bit map
-//        canvas.drawBitmap(smileyBitmap, 500.0f, 200.0f, null);
+    private void setUp() {
+        hexagonPath = new Path();
+        hexagonBorderPath = new Path();
+        maskColor = 0xFFFF0930;
+    }
 
+    public void setRadius(float r) {
+        this.radius = r;
+        calculatePath();
+    }
+
+    public void setMaskColor(int color) {
+        this.maskColor = color;
+        invalidate();
+    }
+
+    private void calculatePath() {
+        float triangleHeight = (float) (Math.sqrt(3) * radius / 2);
+        float centerX = width/2;
+        float centerY = height/2;
+        hexagonPath.moveTo(centerX, centerY + radius);
+        hexagonPath.lineTo(centerX - triangleHeight, centerY + radius/2);
+        hexagonPath.lineTo(centerX - triangleHeight, centerY - radius/2);
+        hexagonPath.lineTo(centerX, centerY - radius);
+        hexagonPath.lineTo(centerX + triangleHeight, centerY - radius/2);
+        hexagonPath.lineTo(centerX + triangleHeight, centerY + radius/2);
+        hexagonPath.moveTo(centerX, centerY + radius);
+
+        float radiusBorder = radius - 2;
+        float triangleBorderHeight = (float) (Math.sqrt(3) * radiusBorder / 2);
+        hexagonBorderPath.moveTo(centerX, centerY + radiusBorder);
+        hexagonBorderPath.lineTo(centerX - triangleBorderHeight, centerY + radiusBorder/2);
+        hexagonBorderPath.lineTo(centerX - triangleBorderHeight, centerY - radiusBorder/2);
+        hexagonBorderPath.lineTo(centerX, centerY - radiusBorder);
+        hexagonBorderPath.lineTo(centerX + triangleBorderHeight, centerY - radiusBorder/2);
+        hexagonBorderPath.lineTo(centerX + triangleBorderHeight, centerY + radiusBorder/2);
+        hexagonBorderPath.moveTo(centerX, centerY + radiusBorder);
+        invalidate();
+    }
+
+
+    // getting the view size and default radius
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        width = 100; //MeasureSpec.getSize(widthMeasureSpec);
+        height =  100; //MeasureSpec.getSize(heightMeasureSpec);
+        radius = height / 2 - 5;
+        calculatePath();
+    }
 }
