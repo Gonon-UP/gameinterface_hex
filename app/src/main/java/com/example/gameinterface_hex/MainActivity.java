f70f330cad2009b98cc32d725ff019416acc16ae
CodyGima <gima27@up.edu>
2024-02-13 00:05:17 -0800

    added title and spinner

--- app/src/main/java/com/example/gameinterface_hex/MainActivity.java
+++ app/src/main/java/com/example/gameinterface_hex/MainActivity.java
@@ -7,26 +7,61 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-    public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+public class MainActivity extends AppCompatActivity {
 
-            TextView myFaveTextView = new TextView(this);
-            myFaveTextView.setText("some text");
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        /**
-         * changeMax
+//            TextView myFaveTextView = new TextView(this);
+//            myFaveTextView.setText("some text");
+
+        /***************************************************************************************
+         *    Title: How to Implement Spinner
+         *    Author: Codes Easy
+         *    Date: 2023
+         *    Availability: https://www.youtube.com/watch?v=4ogzfAipGS8
          *
-         * is called whenever the taps the Update Count button.
-         * It increments the max count by 1.
-         */
+         ***************************************************************************************/
+        Spinner spinner = findViewById(R.id.settings);
+
+        // Use setOnItemSelectedListener instead of setOnItemClickListener
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                // The selected item is selected
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Handle the case where nothing is selected
+            }
+        });
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("How to Play");
+        arrayList.add("Change Color");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arrayList);
+        adapter.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+        spinner.setAdapter(adapter);
+    }
+
+    /**
+     * changeMax
+     *
+     * is called whenever the taps the Update Count button.
+     * It increments the max count by 1.
+     */
 //        public void changeMax(View v) {
 //            //find the TextView that contains the max number
 //            TextView maxTextView = findViewById(R.id.textViewMaxValue);
@@ -48,4 +83,4 @@ import android.widget.TextView;
 //            wesButton.setText("Wes was here");
 //
 //        }
-    }
+}
